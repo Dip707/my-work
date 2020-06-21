193f8384a879868eb0ca3394dde2ad39cc4b69ba
Dip707 <f20190119@gmail.com>
2020-06-21 10:39:04 +0530

    Some Changes

---
+++ color_flipper/app.js
@@ -0,0 +1,14 @@
+function setRandomColor() {
+    document.getElementById('body').style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.getElementById('click').onclick = setRandomColor;
