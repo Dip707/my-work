0318dd996cfb43dee3f628a697e2cc57132c0fa2
Dip707 <f20190119@gmail.com>
2020-06-27 04:33:33 +0530

    done with about and now on project

--- app.js
+++ app.js
@@ -28,22 +28,17 @@
         let aboutItem = document.getElementsByClassName('about_item');
         let aboutDetail = document.getElementsByClassName('about_detail');
 
+
+
         for (let i = 0; i < aboutDetail.length; i++) {
             aboutDetail[i].style.display = "none";
             aboutItem[i].removeAttribute("style");
-
-
         }
+        document.getElementById('about_bio').style.borderLeft = "none";
         aboutDetail[index].style.display = "block";
         styler = document.getElementById(id).style;
         styler.color = "#B3ff00";
         styler.borderLeft = "0.19em solid #B3ff00"
-
-
-
-        // aboutItem[index].style. = "2px";
-
-
     }
 
 
