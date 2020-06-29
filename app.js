08410ae77c6113f6dfb10ff0604c40996cdc63b0
Dip707 <f20190119@gmail.com>
2020-06-29 13:13:08 +0530

    Responsive1

--- app.js
+++ app.js
@@ -1,49 +1,75 @@
-{ //navbar change on scrolling
-    window.onscroll = function() { scrollFunction() };
+{
+    //navbar change on scrolling
+    window.onscroll = function() {
+        scrollFunction();
+    };
 
     function scrollFunction() {
-        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-            document.getElementById('nav').style.height = "4.5em";
-            document.getElementById('nav').style.backgroundColor = "#3d00cf";
-            document.getElementById('logon').style.height = "3.7em";
-            document.getElementById('nav_aboutMe').style.height = "2.3em";
-            document.getElementById('nav_projects').style.height = "2.3em";
-            document.getElementById('nav_sayHello').style.height = "2.3em";
-            document.getElementById('gotop').style.display = "flex";
+        if (
+            document.body.scrollTop > 30 ||
+            document.documentElement.scrollTop > 30
+        ) {
+            document.getElementById("nav").style.height = "4.5em";
+            document.getElementById("nav").style.backgroundColor = "#3d00cf";
+            document.getElementById("logon").style.height = "3.7em";
+            document.getElementById("nav_aboutMe").style.height = "2.3em";
+            document.getElementById("nav_projects").style.height = "2.3em";
+            document.getElementById("nav_sayHello").style.height = "2.3em";
+
             // document.getElementById('').style.height = "2.3em";
-
         } else {
-            document.getElementById('nav').style.height = "5em";
-            document.getElementById('logon').style.height = "3.5em";
-            document.getElementById('nav').style.backgroundColor = "#4a01f3";
-            document.getElementById('nav_aboutMe').style.height = "2.6em";
-            document.getElementById('nav_projects').style.height = "2.6em";
-            document.getElementById('nav_sayHello').style.height = "2.6em";
-            document.getElementById('gotop').style.display = "none";
+            document.getElementById("nav").style.height = "5em";
+            document.getElementById("logon").style.height = "3.5em";
+            document.getElementById("nav").style.backgroundColor = "#4a01f3";
+            document.getElementById("nav_aboutMe").style.height = "2.6em";
+            document.getElementById("nav_projects").style.height = "2.6em";
+            document.getElementById("nav_sayHello").style.height = "2.6em";
 
+        }
+
+        if (
+            document.body.scrollTop > 190 ||
+            document.documentElement.scrollTop > 190
+        ) {
+            document.getElementById('heading').innerHTML = "Dip Turkar";
+            document.getElementById("gotop").style.display = "flex";
+        } else {
+            document.getElementById('heading').innerHTML = "";
+            document.getElementById("gotop").style.display = "none";
         }
     }
 }
 
-{ //creating tabs --myTechnique
+{
+    //creating tabs --myTechnique
     function tabFunction(index, id) {
-        let aboutItem = document.getElementsByClassName('about_item');
-        let aboutDetail = document.getElementsByClassName('about_detail');
-
-
-
+        let aboutItem = document.getElementsByClassName("about_item");
+        let aboutDetail = document.getElementsByClassName("about_detail");
         for (let i = 0; i < aboutDetail.length; i++) {
             aboutDetail[i].style.display = "none";
             aboutItem[i].removeAttribute("style");
         }
-        document.getElementById('about_bio').style.borderLeft = "none";
+        //
         aboutDetail[index].style.display = "block";
         styler = document.getElementById(id).style;
         styler.color = "#B3ff00";
-        styler.borderLeft = "0.19em solid #B3ff00"
+        //
+
+        function myFunction(x) {
+            if (x.matches) {
+                // If media query matches
+                document.getElementById("about_bio").style.borderTop = "none";
+                styler.borderTop = "0.19em solid #B3ff00";
+            } else {
+                document.getElementById("about_bio").style.borderLeft = "none";
+                styler.borderLeft = "0.19em solid #B3ff00";
+            }
+        }
+
+        var x = window.matchMedia("(max-width: 1000px)");
+        myFunction(x);
+        x.addListener(myFunction);
     }
-
-
 }
 
 // { //copy whatsapp number
@@ -53,9 +79,5 @@
 
 //     }
 // }
-
-
-
-
 
 // console.log(x);
